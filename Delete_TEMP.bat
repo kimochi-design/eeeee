3373111b0721c9a967b2431ee94ca07eafe88c17
kimochi-design <kimochi.itaiitai1@gmail.com>
2024-08-28 23:57:09 +0700

    Add files via upload

---
+++ Delete_TEMP.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+title Zen X Cleaner
+color 0C
+
+echo Zen X Cleaner!!
+pause
+
+del /s /f /q %userprofile%\Recent\*.*
+
+del /s /f /q C:\Windows\Prefetch\*.*
+
+del /s /f /q C:\Windows\Temp\*.*
+
+del /s /f /q %USERPROFILE%\appdata\local\temp\*.*
